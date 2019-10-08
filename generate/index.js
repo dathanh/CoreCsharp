f111aad56bd911bd47356fe1bf75cf939b530592
dathanh <datthanh304@gmail.com>
2019-10-08 17:55:34 +0700

    update core

--- generate/index.js
+++ generate/index.js
@@ -5,7 +5,66 @@ const dir = require('./dir');
 const source = '<div>{{title}}</div>';
 
 console.log(dir);
-// exec("node ./Command/CreateEntity create customer",function(data){
-//     console.log(dbConfig);
-// });
+exec("node ./Command/CreateEntity create", function (data) {
+    console.log('CreateEntity done');
+});
+exec("node ./Command/AppendSql create", function (data) {
+    console.log('AppendSql done');
+});
+exec("node ./Command/CreateBR create", function (data) {
+    console.log('CreateBR done');
+});
+exec("node ./Command/CreateControllerCs create", function (data) {
+    console.log('CreateControllerCs done');
+});
+exec("node ./Command/CreateEntityMap create", function (data) {
+    console.log('CreateEntityMap done');
+});
+exec("node ./Command/CreateGridVo create", function (data) {
+    console.log('CreateGridVo done');
+});
+exec("node ./Command/CreateIRepositories create", function (data) {
+    console.log('CreateIRepositories done');
+});
+exec("node ./Command/CreateIService create", function (data) {
+    console.log('CreateIService done');
+});
+exec("node ./Command/CreateMapping create", function (data) {
+    console.log('CreateMapping done');
+});
+exec("node ./Command/CreateModelData create", function (data) {
+    console.log('CreateModelData done');
+});
+exec("node ./Command/CreateModelIndex create", function (data) {
+    console.log('CreateModelIndex done');
+});
+exec("node ./Command/CreateModelPara create", function (data) {
+    console.log('CreateModelPara done');
+});
+exec("node ./Command/CreateModelShare create", function (data) {
+    console.log('CreateModelShare done');
+});
+exec("node ./Command/CreateRepositories create", function (data) {
+    console.log('CreateRepositories done');
+});
+exec("node ./Command/CreateService create", function (data) {
+    console.log('CreateService done');
+});
+exec("node ./Command/CreateViewIndex create", function (data) {
+    console.log('CreateViewIndex done');
+});
+exec("node ./Command/CreateViewShare create", function (data) {
+    console.log('CreateViewShare done');
+});
+
+exec("node ./Command/ImportCoreModule import", function (data) {
+    console.log('ImportCoreModule done');
+});
+exec("node ./Command/ImportTanent import", function (data) {
+    console.log('ImportTanent done');
+});
+exec("node ./Command/ImportWebModule import", function (data) {
+    console.log('ImportWebModule done');
+});
+
 
