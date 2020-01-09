a14199bee175cd19520960ebf4997aa39a7d07ae
dathanh <datthanh304@gmail.com>
2020-01-09 19:08:58 +0700

    add run gen view model

--- generate/index.js
+++ generate/index.js
@@ -5,81 +5,81 @@ const dir = require('./dir');
 const source = '<div>{{title}}</div>';
 
 console.log(dir);
-exec("node ./Command/ImportCoreModule import", function (data) {
+exec("node ./Command/ImportCoreModule import", function(data) {
     console.log('ImportCoreModule done');
 });
-exec("node ./Command/ImportTanent import", function (data) {
+exec("node ./Command/ImportTanent import", function(data) {
     console.log('ImportTanent done');
 });
-exec("node ./Command/ImportWebModule import", function (data) {
+exec("node ./Command/ImportWebModule import", function(data) {
     console.log('ImportWebModule done');
 });
-exec("node ./Command/ImportControllerJs import", function (data) {
+exec("node ./Command/ImportControllerJs import", function(data) {
     console.log('ImportControllerJs done');
 });
-exec("node ./Command/ImportRouterJs import", function (data) {
+exec("node ./Command/ImportRouterJs import", function(data) {
     console.log('ImportRouterJs done');
 });
-exec("node ./Command/ImportDocumentTypeKey import", function (data) {
+exec("node ./Command/ImportDocumentTypeKey import", function(data) {
     console.log('ImportDocumentTypeKey done');
 });
-exec("node ./Command/ImportMenuView import", function (data) {
+exec("node ./Command/ImportMenuView import", function(data) {
     console.log('ImportMenuView done');
 });
-exec("node ./Command/CreateEntity create", function (data) {
+exec("node ./Command/ImportMenuModel import", function(data) {
+    console.log('ImportMenuModel done');
+});
+exec("node ./Command/CreateEntity create", function(data) {
     console.log('CreateEntity done');
 });
-exec("node ./Command/AppendSql create", function (data) {
+exec("node ./Command/AppendSql create", function(data) {
     console.log('AppendSql done');
 });
-exec("node ./Command/CreateBR create", function (data) {
+exec("node ./Command/CreateBR create", function(data) {
     console.log('CreateBR done');
 });
-exec("node ./Command/CreateControllerCs create", function (data) {
+exec("node ./Command/CreateControllerCs create", function(data) {
     console.log('CreateControllerCs done');
 });
-exec("node ./Command/CreateEntityMap create", function (data) {
+exec("node ./Command/CreateEntityMap create", function(data) {
     console.log('CreateEntityMap done');
 });
-exec("node ./Command/CreateGridVo create", function (data) {
+exec("node ./Command/CreateGridVo create", function(data) {
     console.log('CreateGridVo done');
 });
-exec("node ./Command/CreateIRepositories create", function (data) {
+exec("node ./Command/CreateIRepositories create", function(data) {
     console.log('CreateIRepositories done');
 });
-exec("node ./Command/CreateIService create", function (data) {
+exec("node ./Command/CreateIService create", function(data) {
     console.log('CreateIService done');
 });
-exec("node ./Command/CreateMapping create", function (data) {
+exec("node ./Command/CreateMapping create", function(data) {
     console.log('CreateMapping done');
 });
-exec("node ./Command/CreateModelData create", function (data) {
+exec("node ./Command/CreateModelData create", function(data) {
     console.log('CreateModelData done');
 });
-exec("node ./Command/CreateModelIndex create", function (data) {
+exec("node ./Command/CreateModelIndex create", function(data) {
     console.log('CreateModelIndex done');
 });
-exec("node ./Command/CreateModelPara create", function (data) {
+exec("node ./Command/CreateModelPara create", function(data) {
     console.log('CreateModelPara done');
 });
-exec("node ./Command/CreateModelShare create", function (data) {
+exec("node ./Command/CreateModelShare create", function(data) {
     console.log('CreateModelShare done');
 });
-exec("node ./Command/CreateRepositories create", function (data) {
+exec("node ./Command/CreateRepositories create", function(data) {
     console.log('CreateRepositories done');
 });
-exec("node ./Command/CreateService create", function (data) {
+exec("node ./Command/CreateService create", function(data) {
     console.log('CreateService done');
 });
-exec("node ./Command/CreateViewIndex create", function (data) {
+exec("node ./Command/CreateViewIndex create", function(data) {
     console.log('CreateViewIndex done');
 });
-exec("node ./Command/CreateViewShare create", function (data) {
+exec("node ./Command/CreateViewShare create", function(data) {
     console.log('CreateViewShare done');
 });
-exec("node ./Command/CreateControllerJs create", function (data) {
+exec("node ./Command/CreateControllerJs create", function(data) {
     console.log('CreateControllerJs done');
 });
-
-
-
