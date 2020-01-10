a04c9de92ec44f3af8e9ffbc2407958531d56b31
dathanh <datthanh304@gmail.com>
2020-01-10 16:57:29 +0700

    update sql append

--- generate/Command/AppendSql.js
+++ generate/Command/AppendSql.js
@@ -5,28 +5,19 @@ const dir = require('../dir');
 const config = require('config');
 const projectName = config.get('ProjectName');
 const allEntity = require('../Core/AllEntities');
+const _ = require('underscore');
 
 String.prototype.capitalize = function () {
     return this.replace(/(?:^|\s)\S/g, function (a) { return a.toUpperCase(); });
 };
-
-Handlebars.registerHelper('ifCond', function (v1, v2, options) {
-    if (v1 === v2) {
-        return options.fn(this);
-    }
-    return options.inverse(this);
-});
 
 const genDir = __dirname.replace('Command', '')
 
 program.command('create')
     .action(() => {
         allEntity.forEach(entityName => {
-            let isDateTime = false;
-            const fileName = `Shared.cshtml`;
             const entityTemplate = [];
             fs.readFile(genDir + 'Template/Sql.html', function (err, data) {
-                const template = Handlebars.compile(data.toString());
                 const entityFormat = require(genDir + `/Database/${entityName}`);
                 for (var key in entityFormat) {
                     if (!entityFormat[key].includes('virtual')) {
@@ -34,13 +25,13 @@ program.command('create')
                     }
 
                 }
-                var contents = template({
+                var contents = _.template(data.toString())({
                     entityName: entityName.capitalize(),
                     EntityFields: entityTemplate,
                     projectName: projectName,
                 });
                 fs.appendFileSync(dir.Sql, '\n'+ contents);
-                console.log('done');
+                console.log('Append sql done');
             });
         });
 
--- generate/Command/CreateViewShare.js
+++ generate/Command/CreateViewShare.js
@@ -1,6 +1,5 @@
 const program = require('commander');
 const fs = require('fs');
-const Handlebars = require('handlebars');
 const dir = require('../dir');
 const config = require('config');
 const projectName = config.get('ProjectName');
@@ -11,12 +10,6 @@ String.prototype.capitalize = function () {
     return this.replace(/(?:^|\s)\S/g, function (a) { return a.toUpperCase(); });
 };
 
-Handlebars.registerHelper('ifCond', function (v1, v2, options) {
-    if (v1 === v2) {
-        return options.fn(this);
-    }
-    return options.inverse(this);
-});
 
 const genDir = __dirname.replace('Command', '')
 
